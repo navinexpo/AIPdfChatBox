b35735a3a71b059745ba67b8b519029d767fd66c
navinexpo <navinexpo2020@gmail.com>
2026-06-30 18:56:52 +0530

    Update QuiryProvider

--- frontend/src/providers/QueryProvider.tsx
+++ frontend/src/providers/QueryProvider.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
 import { useState } from "react";
-
+// Update QueryProvider
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [client] = useState(
     () =>
